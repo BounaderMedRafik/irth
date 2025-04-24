ab2bb51faa3529dcab4c9f32b5d30bb4fd8083e0
isslamishe <isslamxr8@gmail.com>
2025-04-25 00:04:18 +0100

    .

--- components/core/landing/Hero.tsx
+++ components/core/landing/Hero.tsx
@@ -2,18 +2,24 @@ import React from "react";
 
 const Hero = () => {
   return (
-    <div style={{width:"100%",height:"100vh",backgroundColor:"white",display:"flex",flexDirection:"row"}}>
- <div style={{width:"100%",height:"100%"}}>
- <div  style={{width:"40%",height:"100%",backgroundColor:"",flexDirection:"column",display:"flex",gap:"2vh",paddingLeft:"10%",paddingTop:"4%"}}> 
-    <div style={{display:"flex",flexDirection:"column",fontSize:"250%",fontWeight:"600",gap:"1vh",width:"100%",justifyContent:"start",alignItems:"start"}}>
+    <div style={{   marginTop:"70px",width:"100%",height:"100%",backgroundColor:"white",display:"flex",flexDirection:"row"}}>
+ <div style={{width:"40%",height:"100%",display:"flex",justifyContent:"space-evenly",gap:"20vh",flexDirection:"column"}}>
+ <div  style={{height:"100%",alignItems:"center",flexDirection:"column",display:"flex",gap:"2vh",paddingLeft:"10%",paddingTop:"4%"}}> 
+    <div style={{display:"flex",flexDirection:"column",fontSize:"250%",fontWeight:"600",gap:"1vh",justifyContent:"start",alignItems:"start"}}>
       <p style={{fontFamily:"Inknut Antiqua"}}>Preserving Heritage</p><p style={{fontSize:"100%",fontWeight:"600",fontFamily:"Inknut Antiqua"}}>With Intelligence</p>
       </div>
   <div  style={{fontWeight:"400",fontSize:"2vh"}}>AI-powered exploration of cultural legacy </div>
   <div  style={{display:"flex", gap:'2vh'}}><button style={{backgroundColor:"#D7FCD4",padding:"1%",width:"17vh",height:"auto",fontWeight:"600",borderRadius:"8px",}}>Start Exploring</button> <button style={{backgroundColor:"transparent",border:"1px solid black",padding:"1%",width:"17vh",height:"auto",fontWeight:"600",borderRadius:"8px",}}>Chat with IRTH</button> </div>
   </div>
+  <div style={{display:"flex",width:"100%",height:"20vh",justifyContent:"center",alignItems:"center"}}>
+  <div style={{display:"flex",justifyContent:"center",flexDirection:"column",fontWeight:"500"}}><img src="landing imgs/logo 1.png"/><p>AI Assistent</p></div>
+  <div><img src='landing imgs/icon hirtage 1.png'/><p>Cultural Heritage</p></div>
+  </div>
  </div>
 
-  <div  style={{width:"50%",height:"100%",backgroundColor:"blue"}}><img style={{scale:"0.8"}} src ="Untitled-4 1.png"/></div>
+  <div  style={{width:"60%",height:"100%",backgroundColor:""}}><img style={{scale:"0.8"}} src ="landing imgs/Untitled-4 1.png"/>
+
+  </div>
     </div>
   );
 };
